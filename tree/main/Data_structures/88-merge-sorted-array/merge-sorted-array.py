38b21e5dbd36b541837132e545158a8c45cd06d1
Amaan Khan <amaankhxn2000@gmail.com>
2026-08-15 16:54:50 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (38.31%) - LeetSync

--- tree/main/Data_structures/88-merge-sorted-array/merge-sorted-array.py
+++ tree/main/Data_structures/88-merge-sorted-array/merge-sorted-array.py
@@ -3,21 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        a,b,main_index = m-1,n-1,m+n-1
+        a,b = m-1,n-1
 
-        while main_index >=0:
+        for i in range(m+n-1,-1,-1):
             if a<0:
-                nums1[main_index] = nums2[b]
+                nums1[i] = nums2[b]
                 b-=1
             elif b<0:
                 break
             elif nums1[a] > nums2[b]:
-                nums1[main_index] = nums1[a]
+                nums1[i] = nums1[a]
                 a-=1
             else:
-                nums1[main_index] = nums2[b]
+                nums1[i] = nums2[b]
                 b-=1
-            main_index -=1
 
 
 

5dc5de9e9871420909c1f7adb9b7d4aa40b1fb28
Amaan Khan <amaankhxn2000@gmail.com>
2026-08-17 13:00:16 -0700

    Time: 10 ms (57.38%) | Memory: 22.8 MB (59.98%) - LeetSync

--- tree/main/Data_structures/23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ tree/main/Data_structures/23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,35 +5,40 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists or len(lists) == 0:
+        if not lists or len(lists)==0:
             return None
 
         while len(lists)>1:
-            mergedList = []
-            for i in range(0, len(lists),2):
+            mergedLists=[]
+            for i in range(0,len(lists),2):
                 l1 = lists[i]
-                l2 = lists[i+1] if i+1 < len(lists) else None
-                mergedList.append(self.mergeTwoLists(l1,l2))
-            lists = mergedList
-        return lists[0]    
+                l2 = lists[i+1] if i+1<len(lists) else None
+                mergedLists.append(self.mergeTwoLists(l1,l2))
+            lists = mergedLists
+        return lists[0]
 
-    def mergeTwoLists(self, head1,head2):
+    def mergeTwoLists(self, list1,list2):
         dummy = ListNode()
         curr = dummy
 
-        while head1 and head2:
-            if head1.val > head2.val:
-                curr.next = head2
-                head2 = head2.next
+        while list1 and list2:
+            if list1.val<list2.val:
+                curr.next = list1
+                list1 = list1.next
             else:
-                curr.next = head1
-                head1 = head1.next
-            curr = curr.next    
-        if head1:
-            curr.next = head1
-        if head2:
-            curr.next = head2
-        return dummy.next        
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+
+        if list1:
+            curr.next = list1
+        if list2:
+            curr.next = list2
+        return dummy.next                
+
+
+
+
 
 
 

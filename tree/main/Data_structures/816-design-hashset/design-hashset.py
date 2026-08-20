75f762d58591a494aaf4e4412824cc71cf07d6ef
Amaan Khan <amaankhxn2000@gmail.com>
2026-08-20 11:33:25 -0700

    Time: 46 ms (48.83%) | Memory: 45.8 MB (26.39%) - LeetSync

--- tree/main/Data_structures/816-design-hashset/design-hashset.py
+++ tree/main/Data_structures/816-design-hashset/design-hashset.py
@@ -1,26 +1,15 @@
 class MyHashSet:
 
     def __init__(self):
-        # Create 1000 buckets, each starting as an empty list
-        self.num_buckets = 1000
-        self.buckets = [[] for _ in range(self.num_buckets)]
+        self.mp=[False]*1000001
         
-    def _get_hash(self, key: int) -> int:
-        # Instantly finds the correct bucket index for any number
-        return key % self.num_buckets
 
     def add(self, key: int) -> None:
-        bucket = self.buckets[self._get_hash(key)]
-        # Fix 1: Only add if it's not already there to prevent duplicates
-        if key not in bucket:
-            bucket.append(key)
+        self.mp[key]=True
+        
 
     def remove(self, key: int) -> None:
-        bucket = self.buckets[self._get_hash(key)]
-        # Fix 2: Check first so the code never crashes on missing keys
-        if key in bucket:
-            bucket.remove(key)
+        self.mp[key]=False
 
     def contains(self, key: int) -> bool:
-        bucket = self.buckets[self._get_hash(key)]
-        return key in bucket
+        return self.mp[key]

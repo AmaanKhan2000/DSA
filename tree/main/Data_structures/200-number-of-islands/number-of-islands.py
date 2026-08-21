63b6616062e52e90aac1708bdb4b312a4e13b859
Amaan Khan <amaankhxn2000@gmail.com>
2026-08-21 10:37:44 -0700

    Time: 239 ms (69.81%) | Memory: 21.6 MB (47.74%) - LeetSync

---
+++ tree/main/Data_structures/200-number-of-islands/number-of-islands.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        no_islands = 0
+        ROWS, COLS = len(grid), len(grid[0])
+
+        def dfs(i,j):
+            if i<0 or j<0 or i>=ROWS or j>=COLS or grid[i][j] != '1':
+                return
+            else:
+                grid [i][j] = '0'
+                dfs(i+1,j)
+                dfs(i-1,j)
+                dfs(i,j+1)
+                dfs(i,j-1)
+
+        for m in range(ROWS):
+            for n in range(COLS):
+                if grid[m][n] == '1':
+                    no_islands+=1
+                    dfs(m,n)
+        return no_islands            
+
+
+        

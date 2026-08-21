f649ed903ec9e3ba089eef1b1f676437ffdd3ee6
Amaan Khan <amaankhxn2000@gmail.com>
2026-08-20 18:16:13 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (27.99%) - LeetSync

---
+++ tree/main/Data_structures/78-subsets/subsets.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        sol,res = [],[]
+        n = len(nums)
+
+        def backtracking(i):
+            if i == n:
+                res.append(sol.copy())
+                return
+
+            backtracking(i+1)
+
+            sol.append(nums[i])
+            backtracking(i+1)
+            sol.pop()    
+
+        backtracking(0)
+        return res    
+
+
+        
